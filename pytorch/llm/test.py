700b270bebd727da16a6611b18fdf4f4973c5177
Honglin Zhu <zhuhonglin.zhl@alibaba-inc.com>
2023-05-05 11:04:47 +0800

    llm code

---
+++ pytorch/llm/test.py
@@ -0,0 +1,24 @@
+import time
+import os
+import torch
+from transformers import pipeline
+
+
+def run_model(name, num, inference):
+    with torch.autocast(device_type="cpu", dtype=torch.bfloat16):
+        inference()  # warmup
+        t0 = time.time()
+        for i in range(num):
+            inference()
+        print(name + ' ' + str(num / (time.time() - t0)))
+
+# print(os.getpid())
+# time.sleep(6)
+
+unmasker = pipeline('fill-mask', model='bert-base-uncased')
+run_model('bert-base-uncased', 1000,
+          lambda: unmasker('Paris is the [MASK] of France.'))
+
+# generator = pipeline('text-generation', model='gpt2')
+# run_model('gpt2', 10, lambda: generator(
+#     'Once upon a time,', max_length=30, num_return_sequences=5))
